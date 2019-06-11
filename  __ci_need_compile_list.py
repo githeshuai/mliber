1be5252bbdacb0b4ef7103be555840da7b71e567
heshuai <276575758@qq.com>
2019-06-11 20:32:16 +0800

    add need compile list

---
+++  __ci_need_compile_list.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+compile_files = [
+    "mliber_widgets/asset_widget/asset_delegate.py",
+    "mliber_qt_components/image_sequence_widget.py",
+    "mliber_libs/qt_libs/image_server.py",
+    "mliber_widgets/tag_widget/tag_delegate.py",
+    "mliber_widgets/library_manage/library_manage_delegate.py",
+    "mliber_widgets/main_widget/main_widget.py"
+]
